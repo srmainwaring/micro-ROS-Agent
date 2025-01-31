6756722ee304c9c171a3266466b9b180cdb45666
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-01-31 11:22:42 +0000

    Patch xrceagent for macOS
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- micro_ros_agent/cmake/SuperBuild.cmake
+++ micro_ros_agent/cmake/SuperBuild.cmake
@@ -24,9 +24,9 @@ find_package(xrceagent 2 EXACT QUIET)
 if(NOT xrceagent_FOUND)
     ExternalProject_Add(xrceagent
             GIT_REPOSITORY
-                https://github.com/eProsima/Micro-XRCE-DDS-Agent.git
+                https://github.com/srmainwaring/Micro-XRCE-DDS-Agent.git
             GIT_TAG
-                v2.4.3
+                prs/pr-2.4.3-macos
             PREFIX
                 ${PROJECT_BINARY_DIR}/agent
             INSTALL_DIR
